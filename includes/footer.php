2cd3a524397210f6faeee742d6f4d0d10fd097a6
Joe Fox <joe@jwfoxjr.com>
2011-08-08 13:47:48 -0400

    Created footer.php Reverted header.php to HTML code, rather than extraneous PHP code. Added @todo: comments to header.php and footer.php

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * File name: footer.php
+ * User: joe@jwfoxjr.com
+ * Date: 8/8/11
+ * Time: 1:33 PM
+ *
+ * 
+ * @todo: Add additional footer code.
+ */
+
+?>
+
+</body>
+</html>
--- includes/header.php
+++ includes/header.php
@@ -4,13 +4,16 @@
  * User: joe@jwfoxjr.com
  * Date: 8/8/11
  * Time: 11:06 AM
+ *
+ * 
+ * @todo: add additional header code
  */
 
-echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"\n";
-echo "        \"http://www.w3.org/TR/html4/loose.dtd\">\n";
-echo "<html>\n";
-echo "<head>\n";
-echo "    <title>Fund Drive </title>\n";
-echo "</head>\n";
-
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+  <title></title>
+</head>
+<body>
