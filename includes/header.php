8a30ee3742ec277a2fb49dcf2a89f6bd947e1f49
Joe Fox <joe@jwfoxjr.com>
2011-08-08 11:53:55 -0400

    Started working on header of application. Will add @todo later.

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * File name: header.php
+ * User: joe@jwfoxjr.com
+ * Date: 8/8/11
+ * Time: 11:06 AM
+ */
+
+echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"\n";
+echo "        \"http://www.w3.org/TR/html4/loose.dtd\">\n";
+echo "<html>\n";
+echo "<head>\n";
+echo "    <title>Fund Drive </title>\n";
+echo "</head>\n";
+
+?>
