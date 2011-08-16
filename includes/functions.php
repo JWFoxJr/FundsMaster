8d858e6d599153e2e5e2188e752d07c3e6944a4d
Joe Fox <jwfoxjr@JF167VL.(none)>
2011-08-16 15:41:03 -0400

    Started to create some rudimentary functions to work on for the different aspects of the application.

---
+++ includes/functions.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * File name: functions.php
+ * User: joe@jwfoxjr.com
+ * Date: 8/15/11
+ * Time: 9:20 AM
+ *
+ *
+ * @todo: Add additional functions code.
+ */
+
+function ManageAddress($change_method) {
+
+}
+
+function AddRoute() {
+
+}
+
+function UpdateRoutes() {
+
+}
+
+function EntryMethod($entry_method) {
+
+}
+
+function GenerateMailing($mailing) {
+
+}
+
+
