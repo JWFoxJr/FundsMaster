d7b0adb44da289ff674c0d5adfbc0b28b6a1b1f4
Joe Fox <joe@jwfoxjr.com>
2011-08-26 16:29:47 -0400

    Created function ListRoutes()

--- includes/functions.php
+++ includes/functions.php
@@ -62,8 +62,56 @@ catch(PDOException $e)
 
 }
 
-function AddRoute() {
+function ListRoutes() {
 
+        /*** mysql hostname ***/
+        $hostname = "localhost";
+
+        /*** mysql username ***/
+        $username = "root";
+
+        /*** mysql password ***/
+        $password = "#--5ql4dm1n--#";
+
+        try {
+        $dbh = new PDO("mysql:host=$hostname;dbname=fundsmaster", $username, $password);
+
+        /*** echo a message saying we have connected ***/
+        /* echo "Connected to database<br>"; */
+
+
+        /*** prepare the SQL statement ***/
+        $sql = "SELECT description FROM routes ORDER BY rtcode ASC";
+
+        /*** fetch into an PDOStatement object ***/
+        $stmt = $dbh->query($sql);
+
+        /*** echo number of columns ***/
+        $result = $stmt->fetchALL(PDO::FETCH_COLUMN,0);
+
+        /* echo "<br>";
+                echo round(memory_get_usage() / (1024*1024),3) .' MB<br><br>';*/
+
+        /*** loop of the results ***/
+
+        echo "<select>";
+        foreach($result as $routes){
+            echo "<option name=\"".$routes."\">".$routes."</option>";
+        }
+        unset($routes);
+        echo "</select><br>";
+
+        /* $count = $stmt->rowCount();
+               print ("$count streets \n"); */
+
+        /*** close the database connection ***/
+        $dbh = null;
+
+            }
+    catch(PDOException $e)
+        {
+        echo $e->getMessage();
+        }
 }
 
 function UpdateRoutes() {
--- manageaddresses.php
+++ manageaddresses.php
@@ -25,41 +25,38 @@ include('includes/sidebar.php');
         <div id="body">
             <form method="post" action="<?php echo htmlentities("$_SERVER[PHP_SELF]");?>">
                 <input type="hidden" id="addrmgmt" name="addrmgmt" value="1">
-                <label for="resident">Resident:</label>
+                <label for="resident">Resident:</label><br>
                 <input type="text" id="resident" name="resident" maxlength="30" size="30"
                        onblur="validateLength(1, 32, this, document.getElementById('message_help'))"> <sup>*</sup><br>
                     <sup><span id="message_help" class="help"></span></sup>
-                <label for="house">House Number:</label>
+                <label for="house">House Number:</label><br>
                 <input type="text" id="house" name="house" maxlength="4" size=4
                        onblur="validateNonEmpty(this, document.getElementById('house_help'))"> <sup>*</sup><br>
                     <span id="house_help" class="help"></span>
-                <label for="street">Street:</label>
+                <label for="street">Street:</label><br>
                 <!--<input type="text" id="street" name="street" maxlength="50" size="50"
                        onblur="validateNonEmpty(this, document.getElementById('street_help'));"> <sup>*</sup><br>
                     <span id="street_help" class="help"></span>-->
                 <?php ListStreets(); ?>
-                <label for="suffix">Suffix:</label>
+                <label for="suffix">Suffix:</label><br>
                 <input type="text" id="suffix" name="suffix" maxlength="4"><br>
-                <label for="apt">Apartment:</label>
+                <label for="apt">Apartment:</label><br>
                 <input type="text" id="apt" name="apt" maxlength="4"><br>
-                <label for="city">City:</label>
+                <label for="city">City:</label><br>
                 <input type="text" id="city" name="city" maxlength="30" onblur="validateNonEmpty(this);"> <sup>*</sup><br>
-                <label for="state">State:</label>
+                <label for="state">State:</label><br>
                 <input type="text" id="state" name="state" maxlength="2" onblur="validateNonEmpty(this);"> <sup>*</sup><br>
-                <label for="zip">ZIP:</label>
+                <label for="zip">ZIP:</label><br>
                 <select name="zip" id="zip">
                     <option id="1">14224</option>
                     <option id="2">14218</option>
                     <option id="3">14127</option>
                 </select> <sup>*</sup><br>
-                <label for="plus4">PLUS4:</label>
+                <label for="plus4">PLUS4:</label><br>
                 <input type="text" id="plus4" name="plus4" maxlength="4"><br>
-                <label for="rtcode">Route Code:</label>
-                <select name="rtcode" id="rtcode">
-                    <option value="1">1 - Allendale Road</option>
-                    <option value="2">2 - Orchard Park Road</option>
-                    <option value="3">3 - Bradwood Road / Erwin Road</option>
-                </select><br><br>
+                <label for="rtcode">Route Code:</label><br>
+                <?php ListRoutes(); ?>
+                <br><br>
                 <div style="text-align: center; width: 80%"><input type="submit" name="add" value="Add Address"></div>
                 <br>
                 <sup>*</sup> Required Field
