c196919daf87e148646e735b2521305e763ee835
Joe Fox <joe@jwfoxjr.com>
2011-09-09 21:12:28 -0400

    Added a newline at the end of the file.

--- includes/ajax.php
+++ includes/ajax.php
@@ -14,7 +14,7 @@ Db::setConnectionInfo('fundsmaster','root','#--5ql4dm1n--#','mysql','localhost')
 
     $q=$_GET['q'];
 
-//    /*** prepare the SQL statement ***/
+    /*** prepare the SQL statement ***/
     $sql = 'SELECT house, street, apt, id FROM address WHERE street="'.$q.'" ORDER BY house,apt ASC';
     $result = Db::getResult($sql);
     echo '<select size="5">';
@@ -22,3 +22,4 @@ Db::setConnectionInfo('fundsmaster','root','#--5ql4dm1n--#','mysql','localhost')
     foreach($result as $key=>$house){
         echo '<option name="'.$house['id'].'" value="'.$house['id'].'">'.$house['house'].' '.$house['street'].' '.$house['apt'].'</option>';
     }
+
