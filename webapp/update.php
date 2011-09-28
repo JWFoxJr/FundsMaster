cee2aca4cde025300ee36ea7a514e0f9e319e6ab
Joe Fox <joe@jwfoxjr.com>
2011-09-28 17:22:07 -0400

    Added logic for updating the resident.

--- webapp/update.php
+++ webapp/update.php
@@ -4,15 +4,21 @@
 ?>
     <div id="body">
         <form method="post" action="<?php htmlentities("$_SERVER[PHP_SELF]"); ?>">
-              <label for="resident">Resident:</label><br>
-              <input type="text" id="resident" name="<?php echo $_POST['lstSelectHouse']; ?>" maxlength="30" size="30" value="<?php ShowResident(); ?>">
-              <input type="submit" name="btnChangeResident" value="Change Resident">
+            <input type="hidden" name="txtResidentId" value="<?php echo $_POST['lstSelectHouse']; ?>">
+            <label for="txtChangeResident">Resident:</label><br>
+            <input type="text" id="txtChangeResident" name="txtChangeResident" maxlength="30" size="30" value="<?php ShowResident(); ?>">
+            <input type="submit" name="btnChangeResident" value="Change Resident">
         </form>
     </div>
 
 
 <?php        
-
+    } elseif (isset($_POST['btnChangeResident']) == 'btnChangeResident') {
+        echo 'We will have a function here that will update the record in the database. For the time being we are going to just <br>';
+        echo 'show the posted variable.<br>';
+        echo $_POST['txtResidentId'].'<br>';
+        echo $_POST['txtChangeResident'];
+        
     } else { ?>
 
     <script type="text/javascript">
